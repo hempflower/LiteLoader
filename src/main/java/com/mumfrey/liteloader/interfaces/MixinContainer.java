089d8d450cff863e530fbeaf43fb8aeca1cf4238
Mumfrey <adam@eq2.co.uk>
2017-09-04 18:19:39 +0100

    Catch and report mixin registration errors as critical errors in the GUI

--- src/main/java/com/mumfrey/liteloader/interfaces/MixinContainer.java
+++ src/main/java/com/mumfrey/liteloader/interfaces/MixinContainer.java
@@ -5,6 +5,7 @@
  */
 package com.mumfrey.liteloader.interfaces;
 
+import java.util.Collection;
 import java.util.Set;
 
 public interface MixinContainer<L> extends Loadable<L>, Injectable
@@ -19,5 +20,17 @@ public interface MixinContainer<L> extends Loadable<L>, Injectable
      * Get this mod's list of mixin configs
      */
     public abstract Set<String> getMixinConfigs();
+    
+    /**
+     * Register a mixin error with the container
+     * 
+     * @param th caught throwable
+     */
+    public abstract void registerMixinError(Throwable th);
+    
+    /**
+     * @return mixin errors
+     */
+    public abstract Collection<Throwable> getMixinErrors();
 
 }
